8a8e9f59e725dc1f5cbb8686925cf28f300c50d9
a.rubashka <a.rubashka@godeltech.com>
2023-03-17 15:32:00 +0100

    feat: add common types and update all configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import About from './pages/About/About';
+import MainPage from './pages/Main/MainPage';
+import NotFoundPage from './pages/NotFound/NotFoundPage';
+import { users } from './constants/products.constant';
+
 function App() {
-  return <div>Hello world</div>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <MainPage users={users} />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
