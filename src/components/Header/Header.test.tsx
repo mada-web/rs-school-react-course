9e948ef087f6b4c9b5708a7df5a88889927873c9
a.rubashka <a.rubashka@godeltech.com>
2023-03-26 12:08:37 +0200

    feat: update input component and header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { describe, it } from 'vitest';
-
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+
 import Header from './Header';
 
 describe('Header', () => {
-  it('should render Main and About Us links', () => {
-    render(
+  it('should render all links', () => {
+    const { getByText, getAllByRole } = render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>
     );
 
-    const mainLink = screen.getByText('Main');
-    const aboutUsLink = screen.getByText('About Us');
+    const links = getAllByRole('link');
 
-    expect(mainLink).toBeInTheDocument();
-    expect(aboutUsLink).toBeInTheDocument();
+    expect(links).toHaveLength(3);
+    expect(getByText('Main')).toBeInTheDocument();
+    expect(getByText('Forms')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
   });
 
   it('should have the active class on the active link', () => {
-    render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/about']}>
         <Header />
       </MemoryRouter>
     );
 
-    const mainLink = screen.getByText('Main');
-    const aboutUsLink = screen.getByText('About Us');
+    const mainLink = getByText('Main');
+    const aboutUsLink = getByText('About');
 
     expect(mainLink).not.toHaveClass('activeNavLink');
     expect(aboutUsLink).toHaveClass('activeNavLink');
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,38 +1,51 @@
 import React, { RefObject } from 'react';
 
+import css from './Input.module.css';
+
 export interface IInput {
   id: string;
-  ref?: RefObject<HTMLInputElement>;
-  min?: string;
-  max?: string;
   type: string;
   name: string;
   value?: string;
   label?: string;
-  className?: string;
+  accept?: string;
   placeholder?: string;
+  defaultValue?: number;
+  className?: string;
   labelClassName?: string;
+  wrapperClassName?: string;
+  refProp?: RefObject<HTMLInputElement>;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  errors?: {
+    [key: string]: string;
+  };
 }
 
 class Input extends React.Component<IInput> {
   render() {
     return (
-      <>
-        <label htmlFor={this.props.id} className={this.props.labelClassName}>
-          {this.props.label}
+      <div className={css.InputContainer}>
+        <div className={this.props.wrapperClassName}>
+          <label htmlFor={this.props.id} className={this.props.labelClassName}>
+            {this.props.label}
+          </label>
           <input
             id={this.props.id}
-            ref={this.props.ref}
+            ref={this.props.refProp}
             name={this.props.name}
+            accept={this.props.accept}
             type={this.props.type}
             value={this.props.value}
             className={this.props.className}
+            defaultValue={this.props.defaultValue}
             placeholder={this.props.placeholder}
             onChange={(event) => this.props.onChange && this.props.onChange(event)}
           />
-        </label>
-      </>
+        </div>
+        {this.props?.errors && this.props?.errors[this.props.name] && (
+          <p className={css.Errors}>{this.props.errors[this.props.name]}</p>
+        )}
+      </div>
     );
   }
 }
