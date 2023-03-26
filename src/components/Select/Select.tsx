8814785cfdc4cf8edaa15b13de0213ad9f1f2c26
a.rubashka <a.rubashka@godeltech.com>
2023-03-26 12:09:41 +0200

    feat: add select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 
 import css from './Select.module.css';
 
-class Select extends React.Component {
+class Select extends React.Component<{
+  refProp?: RefObject<HTMLSelectElement>;
+  errors: {
+    [key: string]: string;
+  };
+}> {
   labelText = 'Choose your favorite: ';
 
   render() {
     return (
-      <>
-        <label htmlFor="technologies">
-          {this.labelText}
-          <select name="technologies" id="technologies" className={css.Select}>
-            <option value="vue">Vue</option>
-            <option value="angular">Angular</option>
-            <option value="react">React</option>
-            <option value="next">Next</option>
-            <option value="express">Express.JS</option>
-            <option value="nest">Nest</option>
+      <div className={css.Wrapper}>
+        <div className={css.SelectContainer}>
+          <label htmlFor="technology" className={css.Label}>
+            {this.labelText}
+          </label>
+          <select
+            name="technology"
+            id="technology"
+            className={css.Select}
+            ref={this.props.refProp}
+            placeholder={'Choose...'}
+          >
+            <option value="Choose">Choose...</option>
+            <option value="Vue">Vue</option>
+            <option value="Angular">Angular</option>
+            <option value="React">React</option>
+            <option value="Next">Next</option>
+            <option value="Express">Express</option>
+            <option value="Nest">Nest</option>
           </select>
-        </label>
-      </>
+        </div>
+        {this.props?.errors && this.props?.errors['technology'] && (
+          <p className={css.Errors}>{this.props.errors['technology']}</p>
+        )}
+      </div>
     );
   }
 }
