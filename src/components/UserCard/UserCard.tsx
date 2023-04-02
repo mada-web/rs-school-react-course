2177bb4b3493d77470164ccade1ed400369236fd
a.rubashka <a.rubashka@godeltech.com>
2023-04-02 12:07:37 +0200

    refactor: migrate userCard component to function

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,63 +1,56 @@
-import React from 'react';
-import { IUser } from '../../types/common.types';
+import React, { FC } from 'react';
+
+import { IUserCard } from 'types';
 
 import css from './UserCard.module.css';
 
-class UserCard extends React.Component<{ user: IUser }> {
-  render() {
-    return (
-      <div className={css.CardContainer}>
+export const UserCard: FC<IUserCard> = ({ user }): JSX.Element => {
+  return (
+    <div className={css.CardContainer}>
+      <div className={css.BlockWrapper}>
+        <img src={user.photo} alt={`${user.username} photo`} className={css.Photo} />
+      </div>
+      <div className={css.UserMainInfo}>
         <div className={css.BlockWrapper}>
-          <img
-            src={this.props.user.photo}
-            alt={`${this.props.user.username} photo`}
-            className={css.Photo}
-          />
+          <span>
+            <b>Name: </b>
+            {user.name}
+          </span>
+          <span>
+            <b>Username: </b>
+            {user.username}
+          </span>
         </div>
-        <div className={css.UserMainInfo}>
-          <div className={css.BlockWrapper}>
-            <span>
-              <b>Name: </b>
-              {this.props.user.name}
-            </span>
-            <span>
-              <b>Username: </b>
-              {this.props.user.username}
-            </span>
-          </div>
-          <div className={css.BlockWrapper}>
-            <span>
-              <b>Street: </b>
-              {this.props.user.address.street}
-            </span>
-            <span>
-              <b>Suite: </b>
-              {this.props.user.address.suite}
-            </span>
-            <span>
-              <b>City: </b>
-              {this.props.user.address.city}
-            </span>
-            <span>
-              <b>Zipcode: </b>
-              {this.props.user.address.zipcode}
-            </span>
-          </div>
+        <div className={css.BlockWrapper}>
+          <span>
+            <b>Street: </b>
+            {user.address.street}
+          </span>
+          <span>
+            <b>Suite: </b>
+            {user.address.suite}
+          </span>
+          <span>
+            <b>City: </b>
+            {user.address.city}
+          </span>
+          <span>
+            <b>Zipcode: </b>
+            {user.address.zipcode}
+          </span>
+        </div>
 
-          <div className={css.BlockWrapper}>
-            <span>
-              <b>Phone: </b>
-              {this.props.user.phone}
-            </span>
-            <span>
-              <b>Website: </b>
-              {this.props.user.website}
-            </span>
-          </div>
+        <div className={css.BlockWrapper}>
+          <span>
+            <b>Phone: </b>
+            {user.phone}
+          </span>
+          <span>
+            <b>Website: </b>
+            {user.website}
+          </span>
         </div>
       </div>
-    );
-  }
-}
-
-export default UserCard;
+    </div>
+  );
+};
