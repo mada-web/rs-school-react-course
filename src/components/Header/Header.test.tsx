2caf8e203c32b9bae0b924651b18c2b33b99d488
a.rubashka <a.rubashka@godeltech.com>
2023-04-02 12:06:20 +0200

    refactor: migrate header component to function

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,7 +3,7 @@ import { describe, it } from 'vitest';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import Header from './Header';
+import { Header } from './Header';
 
 describe('Header', () => {
   it('should render all links', () => {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import navLinks from './Header.constants';
+
 import css from './Header.module.css';
-import navLinks from '../../constants/navigation.constant';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className={css.Header}>
-        <nav>
-          {navLinks.map((el) => (
-            <NavLink
-              key={el.id}
-              to={el.to}
-              className={({ isActive }) =>
-                isActive ? `${css.activeNavLink} ${css.NavLink}` : css.NavLink
-              }
-            >
-              {el.name}
-            </NavLink>
-          ))}
-        </nav>
-      </header>
-    );
-  }
-}
-
-export default Header;
+export const Header: FC = () => {
+  return (
+    <header className={css.Header}>
+      <nav>
+        {navLinks.map((el) => (
+          <NavLink
+            key={el.id}
+            to={el.to}
+            className={({ isActive }) =>
+              isActive ? `${css.activeNavLink} ${css.NavLink}` : css.NavLink
+            }
+          >
+            {el.name}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
+  );
+};
