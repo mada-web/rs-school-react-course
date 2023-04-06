bace7ea361365d8e311e5e27d985aa7c01d8da6d
a.rubashka <a.rubashka@godeltech.com>
2023-04-06 17:24:47 +0200

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={css.Footer}>
+      <div className={css.Content}>
+        <span>© 2023 Artsem Rubashka</span>
+      </div>
+    </div>
+  );
+};
