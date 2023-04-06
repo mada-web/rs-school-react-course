5f0313daa008626bdf4f491afa45abf936ea2811
a.rubashka <a.rubashka@godeltech.com>
2023-04-06 17:30:19 +0200

    refactor: update styles for main container, header and form components

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Header } from '../Header/';
 
 import css from './Container.module.css';
+import { Footer } from '../Footer';
 
 interface IContainer {
   children: React.ReactNode;
@@ -11,7 +12,8 @@ export const Container: FC<IContainer> = ({ children }) => {
   return (
     <div className={css.Container}>
       <Header />
-      <main>{children}</main>
+      <main className={css.Content}>{children}</main>
+      <Footer />
     </div>
   );
 };
