87ec6731b6c1208514c360ed9e0f01b3b3e6f7a6
a.rubashka <a.rubashka@godeltech.com>
2023-04-25 13:01:06 +0200

    feat: create server for ssr

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import { readFile } from 'fs/promises';
+import { dirname, resolve } from 'path';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { createServer } from 'vite';
+
+const app = express();
+
+const PORT = 8080;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const htmlIndex = resolve(__dirname, '../index.html');
+
+const vite = await createServer({
+  server: { middlewareMode: true },
+  appType: 'custom',
+});
+
+app.use(vite.middlewares);
+
+app.use('*', async (req, res, next) => {
+  const url = req.originalUrl;
+
+  try {
+    const template = await readFile(htmlIndex, 'utf-8');
+
+    const htmlData = await vite.transformIndexHtml(url, template);
+
+    const [htmlStart, htmlEnd] = htmlData.split(`<!--ssr-outlet-->`);
+
+    const { render } = await vite.ssrLoadModule('./src/entry-server.tsx');
+
+    const { pipe } = await render(url, {
+      onShellReady() {
+        res.write(htmlStart);
+        pipe(res);
+      },
+
+      onAllReady() {
+        res.write(htmlEnd);
+        res.end();
+      },
+    });
+  } catch (e) {
+    vite.ssrFixStacktrace(e as Error);
+    next(e);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
